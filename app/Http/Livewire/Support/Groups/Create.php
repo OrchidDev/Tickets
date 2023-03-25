eb85e7657f0595fefd4255467917cbaf1ad6b5c4
RaymondTomcat <swordart2050@gmail.com>
2023-03-26 02:06:01 +0430

    Create group to team collection

--- app/Http/Livewire/Support/Groups/Create.php
+++ app/Http/Livewire/Support/Groups/Create.php
@@ -2,14 +2,61 @@
 
 namespace App\Http\Livewire\Support\Groups;
 
+use App\Models\Group;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Create extends Component
 {
+    use WithFileUploads;
+    public $logo;
+    public Group $group;
     public function render()
     {
         return view('livewire.support.groups.create')
             ->extends('layouts.app')
             ->section('content');
     }
+
+    public function mount()
+    {
+        $this->group = new Group();
+    }
+
+    protected $rules = [
+        'group.name' => 'nullable',
+        'group.type' => 'nullable',
+        'group.logo' => 'nullable',
+    ];
+    public function updated($name)
+    {
+        $this->validateOnly($name);
+    }
+
+    public function storeGroup()
+    {
+        $this->validate();
+        $group = Group::create([
+            'name' => $this->group->name,
+            'type' => $this->group->type,
+        ]);
+
+
+        if ($this->logo) {
+            $group->update([
+                'logo' => $this->uploadImage()
+            ]);
+        }
+
+        $this->dispatchBrowserEvent('toastr:success', ['message' => 'گروه با موفقیت ایجاد شد']);
+        return redirect()->route('groups.index');
+    }
+
+    public function uploadImage()
+    {
+        $year = now()->year; $month = now()->month; $directory = "groups/$year/$month";
+        $name= $this->logo->getClientOriginalName(); $this->logo->storeAs($directory,$name);
+        return "$directory/$name";
+    }
+
 }
--- resources/views/livewire/support/groups/create.blade.php
+++ resources/views/livewire/support/groups/create.blade.php
@@ -2,12 +2,12 @@
 <div class="container">
     <div class="card">
         <div class="card-body">
-            <form role="form" enctype="multipart/form-data">
+            <form role="form" enctype="multipart/form-data" wire:submit.prevent="storeGroup">
                 <div class="row mb-3">
                     <div class="col-md-6">
                         <div class="mb-3">
                             <label for="Input1" class="form-label">نام گروه</label>
-                            <input type="text" class="form-control" id="Input1">
+                            <input type="text" class="form-control" id="Input1" wire:model.defer="group.name">
                             @error('') <span class="text-danger">{{ $message }}</span> @enderror
                         </div>
                     </div>
@@ -15,10 +15,11 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="select1">گروه اصلی</label>
-                            <select class="form-control" id="select1">
+                            <select class="form-control" id="select1" wire:model.defer="group.type">
                                 <option selected>انتخاب کنید ...</option>
-                                <option value="1">تست</option>
-                                <option value="0">تست</option>
+                                @foreach(\App\Models\Group::all() as $group)
+                                    <option value="{{$group->id}}">{{$group->name}}</option>
+                                @endforeach
                             </select>
                         </div>
                     </div>
@@ -26,7 +27,10 @@
                     <div class="col-md-12 mt-3">
                         <label for="file1">لوگو گروه</label>
                         <div class="preview mb-3"></div>
-                        <input class="form-control" type="file" id="file1">
+                        <input class="form-control" type="file" id="file1" wire:model.defer="group.logo">
+                        @if ($logo)
+                            <img src="{{ $logo->temporaryUrl() }}" alt="">
+                        @endif
                     </div>
 
                 </div>
